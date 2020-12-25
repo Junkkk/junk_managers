7ba606dcbd9e4f8f158124b9537d7eb67b802556
Junk <zuzlov260@mail.ru>
2020-12-25 22:20:18 +0300

    add integration tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -2,10 +2,19 @@ import pytest
 import docker
 import socket
 import time
-from psycopg2 import OperationalError
-from sqlalchemy import create_engine
 import sqlalchemy
+import sys
+sys.path = ['', '..'] + sys.path[1:]
+
+from app.models.user.db_model import User
+from app.models.project.db_model import Project
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from fastapi.testclient import TestClient
+from app.main import app
+
+
+client = TestClient(app)
 
 
 def ping_db():
@@ -22,8 +31,8 @@ def ping_db():
         return False
 
 
-@pytest.yield_fixture(scope='function')
-def postgres_db():
+@pytest.fixture(scope='session')
+def postgres_container():
     client = docker.from_env()
     client.containers.run(
         image='postgres',
@@ -33,13 +42,12 @@ def postgres_db():
         environment=["POSTGRES_PASSWORD=postgres", "POSTGRES_DB=test_managers"],
         detach=True
     )
-    print('container run')
     yield client
     client.containers.get('test_managers').stop()
-    print('container stop')
 
 
-def test_db(postgres_db):
+@pytest.fixture(scope='session')
+def postgres_session():
     if ping_db():
         for _ in range(10):
             try:
@@ -49,11 +57,81 @@ def test_db(postgres_db):
                 file = open('tests/filldb.sql')
                 escaped_sql = sqlalchemy.text(file.read())
                 engine.execute(escaped_sql)
-                print('DB created')
+                yield session
                 break
             except Exception as e:
-                print('cant connect\n' + str(e))
                 time.sleep(1)
     else:
         print('Cant connect to db')
-    time.sleep(10)
+    session.close()
+
+
+def test_db(postgres_container, postgres_session):
+    assert len(postgres_session.query(User).all()) == 19
+    assert postgres_session.query(User).filter(User.id == 1).first().name == 'Anton'
+
+
+def test_db2(postgres_session):
+    assert len(postgres_session.query(Project).all()) == 4
+
+
+def test_managers_not_auth():
+    response = client.get('/managers')
+    assert response.status_code == 401
+    assert response.json()['detail'] == 'Not authenticated'
+
+
+def test_managers_owner(postgres_session):
+    user_name = postgres_session.query(User).filter(User.id == 1).first().name
+    user_jwt = client.post(f'/login/access-token?username={user_name}').json()['access_token']
+    response = client.get('/managers', headers={'Authorization': f'Bearer {user_jwt}'})
+    assert response.status_code == 200
+    project_list = ('Umbrella', 'Werwolf')
+    for project in project_list:
+        assert project in response.json()
+
+    managers_list = ('Alena', 'Alisa', 'Almeria', 'Junk')
+    assert len(response.json()['Umbrella']) == 4
+    for manager in managers_list:
+        assert manager in response.json()['Umbrella']
+
+    managers_list = ('Epitone', 'Minami', 'Krabzr')
+    assert len(response.json()['Werwolf']) == 4
+    for manager in managers_list:
+        assert manager in response.json()['Werwolf']
+
+    response = client.get('/managers?q=1', headers={'Authorization': f'Bearer {user_jwt}'})
+    assert response.status_code == 200
+    assert len(response.json()) == 1
+    assert 'Umbrella' in response.json()
+    assert 'Werwolf' not in response.json()
+
+    response = client.get('/managers?q=2', headers={'Authorization': f'Bearer {user_jwt}'})
+    assert response.status_code == 200
+    assert len(response.json()) == 1
+    assert 'Umbrella' not in response.json()
+    assert 'Werwolf' in response.json()
+
+    response = client.get('/managers?q=3', headers={'Authorization': f'Bearer {user_jwt}'})
+    assert response.status_code == 200
+    assert len(response.json()) == 0
+    assert 'Umbrella' not in response.json()
+    assert 'Werwolf' not in response.json()
+
+
+def test_managers_employee(postgres_session):
+    user_name = postgres_session.query(User).filter(User.id == 2).first().name
+    user_jwt = client.post(f'/login/access-token?username={user_name}').json()['access_token']
+    response = client.get('/managers', headers={'Authorization': f'Bearer {user_jwt}'})
+
+    project_list = ('Umbrella', 'Werwolf')
+    for project in project_list:
+        assert project in response.json()
+
+    managers_list = ('Alisa', 'Almeria', 'Junk')
+    assert len(response.json()['Umbrella']) == 3
+    for manager in managers_list:
+        assert manager in response.json()['Umbrella']
+
+    assert len(response.json()['Werwolf']) == 1
+    assert 'Alena' in response.json()['Werwolf']
